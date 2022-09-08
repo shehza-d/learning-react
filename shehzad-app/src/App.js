fbc30dc5d6521f48c491d0493db0d94ff843bf63
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-09-08 21:07:27 +0500

    final

--- shehzad-app/src/App.js
+++ shehzad-app/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <center> <img src="./shehza.d_1___B6BqlPNH9sT___ (2).jpg" width="200" height="200"></center> */}
+      <i> <center> <h1>SHEHZAD IQBAL</h1></center> </i>
+
+      <h2>Digital Resume</h2>
+      <hr />
+      <p>
+        Adress here <br />
+        Karachi <br />
+        03033111488 <br />
+        shehzaddiqbal@outlook.com <br />
+      </p>
+      <hr />
+
+      <h3>Skills</h3>
+      <ul>
+        <li>Web Developer</li>
+        <li>C++</li>
+        <li>Communicating Skills</li>
+        <li>Problem Solving</li>
+        <li>Creative</li>
+        <li>10 Finger Typing (35WPM)</li>
+        <li>Costomer Services</li>
+        <li>MS-Office</li>
+      </ul>
+      <h3>Objective</h3>
+      <p>Join a dynamic and progressive organization offering ample opportunities in diversified exposure where I could
+        use my qualification and skills.
+        Always Honesty with my work.
+      </p>
+      <h3>Experience</h3>
+      <p>1 Year of teaching experience at Sunny Computer
+        Center as an MS-Office and other short course teacher.</p>
+      <h3>Bio Data</h3>
+      <p>Date of Birth = 13-03-2003 <br />
+        Relationship Status = Single <br />
+        Religion = Islam <br />
+        Gender = Male</p> <br />
+      <h3>Education</h3>
+      <p>
+        Matric, — from Farabi School Karachi <br />
+        2020 (Bio)
+        <br />
+        Intermediate , — from FG College Cant <br />
+        (Computer Science) <br />
+        Web Development <br />
+        from Saylani mass IT Traning
+      </p>
+      <br />
+      <br />
+      <a href="https://www.linkedin.com/in/shehzad-iqbal-726512228/">LinkedIN</a> <br />
+      <a href="https://github.com/shehza-d">GitHub</a> <br />
+      <a href="https://www.youtube.com/channel/UCUTMs216kmgY9lOgBFDckAQ">My YouTube Chennnal</a>
     </div>
   );
 }
