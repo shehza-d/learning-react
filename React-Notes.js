6b535a163c0a4850f77188c82d737b2e8dbf4da8
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-09-24 20:15:16 +0500

    Notes Pushed

---
+++ React-Notes.js
@@ -0,0 +1,62 @@
+React Default Cmts
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+
+
+//json placeholder
+//jo chez state variable ma ho gyi wohi display ho gyi aur kuxh bhi display nhi hoga
+//khali array per map nhi kam karta
+
+
+
+assignments
+//make weather app on react
+//apply router on twitter and call api on twitter
+
+
+
+
+
+hook ?
+formik hook for react forms is one of many react form hook app.js ko .jsx nhi karna?
+useEffect ? (conponent load hone ky baad chalta hy)()
+useffect ko jo funciton diya hy f
+
+dependancy array ma jab kuxh change hoga tw tw funciton change hoga
+halka sa type karne per API call karna is bad practise Enter press karne per API call ho
+
+
+array ma img laga deyn jo har refresh per change ho
+lifecycle = kese kya chez chale gyi aur destroy hogyi
+2nd way is the best
+
+
+
+eventLoop!!  = https://www.youtube.com/watch?v=8aGhZQkoFbQ
+
+
+queues
+next tik =
+
+
+Hook is realted to UI rerandering 
+useState ma jab change aata hy tw component rerendar hota hy
+useState vs useEffect
+library is box full of functions
+hook start with use****
+
+
+--save is optional 
+phale --save se package.json ma nam dependance ma nam save hota tha phale
+[formik hook] formik.org
+
+
+yup is a validation library
+
+
+full proper signup ka form formik ky sath
+
+
